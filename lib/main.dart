2c45987374e42a30241efbacc4e53eed90ba9983
Brandon Martinez <brandonam04@outlook.com>
2020-08-13 12:11:57 -0400

    Begun work on layout, added container for navigation bar. TODO: Make the button class for those two, and make them take you to separate views when you tap on each one.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,60 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
+import 'package:flutter/rendering.dart';
+
 void main() => runApp(DialedIn());
 
 class DialedIn extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
+    Widget navSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+            children: [
+              Text('Timer'),
+            ],
+          ),//Timer button column
+          Column(
+            children: [
+              Text('Navigation'),
+            ],
+          ),//History button column
+        ],
+      ),
+    );
     return MaterialApp(
-      title: 'Welcome to dialedIn'
+      title: 'Welcome to dialedIn',
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to dialedIn'),
+        ),
+        body: Column(
+          children: [
+            navSection,
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: RaisedButton(
+        onPressed: () {},
+        child: const Text('bottom button!', style: TextStyle(fontSize:20)),
+        color: Colors.blue,
+        textColor: Colors.white,
+        elevation: 5,
+      ),
     );
   }
 }
