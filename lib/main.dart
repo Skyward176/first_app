9908fa9980c3f660faae712dc151704724614ca9
Brandon Martinez <brandonam04@outlook.com>
2020-08-14 11:33:44 -0400

    Initial commit for the material spec compliant branch of the app

--- lib/main.dart
+++ lib/main.dart
@@ -8,60 +8,12 @@ void main() => runApp(DialedIn());
 class DialedIn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Widget navSection = Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Column(
-            children: [
-              RaisedButton(
-                onPressed: (){},
-                child: const Text('Timer'),
-                color: Colors.grey[900],                
-              ),
-            ],
-          ),//Timer button column
-          Column(
-            children: [
-              RaisedButton(
-                onPressed: () {},
-                child: const Text('Navigation'),
-                color: Colors.grey[900],
-              )
-            ],
-          ),//History button column
-        ],
-      ),
-    );
     return MaterialApp(
-      title: 'Welcome to dialedIn',
+      title:'Welcome to dialedIn',
       theme: ThemeData.dark(),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to dialedIn'),
-        ),
-        body: Column(
-          children: [
-            navSection,
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-
-class Body extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.bottomCenter,
-      child: RaisedButton(
-        onPressed: () {},
-        child: const Text('bottom button!', style: TextStyle(fontSize:20)),
-        color: Colors.blue,
-        textColor: Colors.white,
-        elevation: 5,
+        appBar: AppBar(title: Text('dialedIn')),
+        body: Text('Hello There'),
       ),
     );
   }
